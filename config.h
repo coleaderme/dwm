562c99f61ae9974d9b14a6ea84c8bc74469b7a12
coleaderme <coleaderme@gmail.com>
2023-08-22 23:11:14 +0530

    revert: last change

--- config.h
+++ config.h
@@ -95,9 +95,9 @@ static const Layout layouts[] = {
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 #define STACKKEYS(MOD,ACTION) \
     { MOD,	XK_j,	ACTION##stack,	{.i = INC(+1) } }, \
+    { MOD,  XK_Tab,   ACTION##stack,  {.i = INC(+1) } }, \
     { MOD,	XK_k,	ACTION##stack,	{.i = INC(-1) } }, \
-    { Mod1Mask,  XK_Tab,   ACTION##stack,  {.i = INC(+1) } }, \
-    { Mod1Mask|ShiftMask,  XK_Tab,   ACTION##stack,  {.i = INC(-1) } }, \
+    { MOD|ShiftMask,  XK_Tab,   ACTION##stack,  {.i = INC(-1) } }, \
     { MOD,  XK_v,   ACTION##stack,  {.i = 0 } }, \
     /* { MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \ */
     /* { MOD, XK_a,     ACTION##stack, {.i = 1 } }, \ */
