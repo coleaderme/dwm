07b28328c5f3f640c82aa8313d5431fca321a353
coleaderme <coleaderme@gmail.com>
2024-04-25 11:18:06 +0530

    font size

--- config.h
+++ config.h
@@ -20,8 +20,8 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 0;        /* 0 means bottom bar */
-static char *fonts[]                = { "CaskaydiaCoveNerdFontPropo-Regular:pixelsize=14"};
-static const char dmenufont[]       = "CaskaydiaCoveNerdFontPropo-Regular:pixelsize=14";
+static char *fonts[]                = { "CaskaydiaCoveNerdFontPropo-Regular:pixelsize=12"};
+static const char dmenufont[]       = "CaskaydiaCoveNerdFontPropo-Regular:pixelsize=12";
 static char normbgcolor[]           = "#353535";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#F8F8F2";
